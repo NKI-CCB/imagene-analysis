f3ec0bad726e942dd11d3c224e0c97156b84d9e6
Tycho Bismeijer <t.bismeijer@nki.nl>
2018-02-12 18:24:30 +0100

    Show stronger selection in gsea tables

--- src/reports/es-table-fun.py
+++ src/reports/es-table-fun.py
@@ -4,7 +4,10 @@ def display_table(t):
         tabulate(t, headers='keys') +
         "\n\n</div>"
     ))
-def table_ds(ds, fdr, le_prop=0.0):
+def table_ds(ds, fdr, le_prop=0.0, nes=0.0):
     df = ds.to_dataframe()
     df.reset_index(level=0, inplace=True)
-    display_table(df.loc[(df['fdr'] < fdr) & (df['le_prop'] > le_prop)])
+    df = df.loc[(df['fdr'] < fdr) &
+                (df['le_prop'] > le_prop) &
+                (np.abs(df['nes']) > nes) ]
+    display_table(df)
