f1ac72be54b359ae656ac7bc1d13dc6db8c490ca
Tycho Bismeijer <t.bismeijer@nki.nl>
2018-02-19 13:44:56 +0100

    Annotate genesets in waterfall plots

--- src/visualization/figure-gsea-heatmap.py
+++ src/visualization/figure-gsea-heatmap.py
@@ -1,3 +1,5 @@
+from itertools import count
+
 import click
 import matplotlib
 import numpy as np
@@ -6,10 +8,21 @@ import xarray as xr
 
 from lib import click_utils
 import plot
+from visualization.style import set_style
 
+gs_labels = [chr(c) for c in range(ord('a'), ord('z')+1)]
+gs_label_props = dict(
+    weight='bold',
+    bbox=dict(boxstyle="circle,pad=0.15",
+              facecolor='#333333',
+              edgecolor='none'),
+    color='white',
+    fontsize=9,
+)
 
 def wf_plot(vals, highlight, ax, ylabel="", yscale="linear", ylim=None,
             xbaseline=None, reverse=False):
+    highlight = list(highlight)
     if reverse:
         vals_order = np.argsort(-vals.values)
     else:
@@ -17,11 +30,13 @@ def wf_plot(vals, highlight, ax, ylabel="", yscale="linear", ylim=None,
 
     vals = vals[vals_order]
 
-    x = np.arange(len(vals)) / len(vals) * 100
+    x_grid = np.arange(len(vals)) / len(vals) * 100
 
     hl_mask = np.isin(vals['gene_set'], highlight)
-    x_hl = x[hl_mask]
+    x_hl = x_grid[hl_mask]
     vals_hl = vals[hl_mask]
+    genesets_hl = vals['gene_set'][hl_mask]
+    genesets_idx = [highlight.index(gs) for gs in genesets_hl.values]
 
     y_invert = False
     if yscale == 'mlog10':
@@ -30,7 +45,7 @@ def wf_plot(vals, highlight, ax, ylabel="", yscale="linear", ylim=None,
 
     if xbaseline is None:
         if yscale == 'log':
-            xbaseline = np.max(x)
+            xbaseline = np.max(vals)
         else:
             xbaseline = 0
 
@@ -38,9 +53,23 @@ def wf_plot(vals, highlight, ax, ylabel="", yscale="linear", ylim=None,
     if y_invert:
         ax.invert_yaxis()
 
-    ax.fill_between(x, xbaseline, vals, facecolors='#777777',
+    ax.fill_between(x_grid, xbaseline, vals, facecolors='#777777',
                     step='mid', edgecolors='none')
     ax.vlines(x_hl, xbaseline, vals_hl, colors='#44ff44', linewidth=.8)
+    for x, y, idx, i in zip(x_hl, vals_hl, genesets_idx, count()):
+        y = min(xbaseline, y)
+        a = ax.annotate(
+            xy=(x, y),
+            xycoords='data',
+            s=gs_labels[idx],
+            xytext=(0.5 + 0.095 * i, -8),
+            textcoords=('axes fraction', 'axes points'),
+            arrowprops=dict(facecolor='black', width=0.01, headlength=0.01,
+                            headwidth=0.01, lw=0.5, shrink=0.0),
+            horizontalalignment='center',
+            verticalalignment='center',
+            **gs_label_props,
+        )
 
     if ylim is not None:
         ax.set_ylim(ylim)
@@ -67,10 +96,11 @@ class SFDRNormalize(matplotlib.colors.Normalize):
                        self.vmax]
         norm_points = [0, 0.499, 0.5, 0.501, 1]
         return np.ma.masked_array(np.interp(value, data_points, norm_points))
-#####
 
 
 def plot_gsea_heatmap(gsea, genesets_annot, factor_idx, fig):
+    plusminus_sign = chr(0x00B1)
+
     genesets = genesets_annot['gene_set'].values
     assert all(np.isin(genesets, gsea['gene_set']))
     wf_prop = 0.3
@@ -78,7 +108,7 @@ def plot_gsea_heatmap(gsea, genesets_annot, factor_idx, fig):
     hm_prop = 1-wf_prop-table_prop
     cbar_vmargin = 0.25
     wf_hmargin = 0.05
-    wf_vmargin = 0.01
+    wf_vmargin = 0.06
 
     sel_gsea = gsea.reindex_like(genesets_annot)
 
@@ -103,15 +133,17 @@ def plot_gsea_heatmap(gsea, genesets_annot, factor_idx, fig):
     lfdr_ticks_at = -np.log10(fdr_ticks_at)
     cbar_tick_lv = np.append(np.append(-lfdr_ticks_at[::-1], [0.0]),
                              lfdr_ticks_at)
-    cbar_tick_v = np.append(-np.append(fdr_ticks_at[::-1], [0.0]),
+    cbar_tick_v = np.append(-np.append(fdr_ticks_at[::-1], [1.0]),
                             fdr_ticks_at)
     cbar.set_ticks(cbar_tick_lv)
     ax_cbar.set_xlabel("signed FDR")
-    cbar.ax.set_xticklabels([f"{v}" for v in cbar_tick_v])
+    cbar_tick_labels = [f"{v}" for v in cbar_tick_v]
+    cbar_tick_labels[len(cbar_tick_labels) // 2] = plusminus_sign + "1.0"
+    cbar.ax.set_xticklabels(cbar_tick_labels)
     ax.set_xticklabels("")
     ax.tick_params(bottom='off')
-    ax.set_xlabel("Gene Set")
     ax.set_ylabel("CAD Factor")
+    ax.set_xlabel("")
     ax.spines['top'].set_visible(False)
     ax.spines['bottom'].set_visible(False)
     ax.spines['left'].set_visible(False)
@@ -143,10 +175,11 @@ def plot_gsea_heatmap(gsea, genesets_annot, factor_idx, fig):
         ga['source'] = ('gene_set', [f"{s} ({y})" for s, y in sy])
         del ga['source_year']
     gaa = ga.to_array()
+    xlabels = [gs_labels[i] for i in range(gaa.shape[1])]
     table = ax.table(
-        cellText=gaa.values,
+        cellText=np.vstack((xlabels, gaa.values)),
         cellLoc='center',
-        rowLabels=gaa['variable'].values,
+        rowLabels=['gene set'] + list(gaa['variable'].values),
         loc='bottom',
     )
     table.auto_set_font_size(False)
@@ -156,8 +189,10 @@ def plot_gsea_heatmap(gsea, genesets_annot, factor_idx, fig):
         cell.set_edgecolor('w')
         if col % 2 == 1:
             cell.set_facecolor('#eeeeee')
-        if col == 2:
+        if col == 3:
             cell.set_height(2*cell.get_height())
+        if col == 0 and row >= 0:
+            cell.set_text_props(**gs_label_props)
         if row == -1:
             cell.set_text_props(weight='bold')
 
@@ -184,4 +219,5 @@ def plot_gsea_heatmap_(gsea_results, sel_genesets, factor, out):
 
 
 if __name__ == '__main__':
+    set_style()
     plot_gsea_heatmap_()
